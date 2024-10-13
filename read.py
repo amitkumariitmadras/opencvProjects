9ee9ca8fccabf30cb4ac32e0d45c399873859885
amitkumariitm <amitkumarsumit62027@gmail.com>
2024-10-13 18:00:43 +0530

    read image using opecv- commit 1

---
+++ read.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+
+# Load the image
+img = cv.imread('/Users/amit/Desktop/AadharCode/openCVProjects/Resources/Photos/cat_large.jpg')
+
+if img is None:
+    print("Error: Could not load image. Check the file path.")
+else:
+    cv.imshow("Cat", img)
+
+
+    # Wait for a key event
+    # Press 'q' or Esc (ASCII 27) to exit
+    if cv.waitKey(0) & 0xFF == 27:  # Wait for the Esc key
+        cv.destroyAllWindows()  # Close all windows
